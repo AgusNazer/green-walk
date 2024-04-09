536c05c1fdb63506e57eb28a4d7a074d84b89772
Nicolas <villagrajosenicolas@gmail.com>
2024-04-09 20:44:49 -0300

    feat:Hero

--- client/Vite-Template/src/pages/NavBar/NavBar.tsx
+++ client/Vite-Template/src/pages/NavBar/NavBar.tsx
@@ -5,10 +5,11 @@ const NavBar = () => {
     <nav
   className="fixed z-10 w-full  px-6 py-3 mx-auto text-white ">
   <div className="flex items-center justify-between text-blue-gray-900">
-    <a href="/a"
+    {/* <a href="/a"
       className="mr-4 block cursor-pointer py-1.5 font-sans text-2xl font-semibold leading-relaxed tracking-normal text-inherit antialiased">
       Green Runner
-    </a>
+    </a> */}
+    <img className="sm:w-[7rem] mr-4 block cursor-pointer py-1.5 font-sans font-semibold leading-relaxed tracking-normal text-inherit antialiased" src="/Logogreen.png" alt="logo" />
     <div className="hidden lg:block">
       <ul className="flex flex-col gap-2 my-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
         <li className="block p-1 font-sans text-2xl antialiased font-medium leading-normal text-blue-gray-900">
