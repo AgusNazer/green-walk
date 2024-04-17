754cb2892a3cd89ab815696df665a61f3abb062a
Nicolas <villagrajosenicolas@gmail.com>
2024-04-17 00:18:50 -0300

    feat:update server and feat ux

--- greendly_app/src/pages/Home.jsx
+++ greendly_app/src/pages/Home.jsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
 import { View, StyleSheet, TouchableOpacity, Text , Button , Alert } from "react-native";
 import { Video } from "expo-av";
 import { useNavigation } from "@react-navigation/native";
 import axios from 'axios'
+import * as Location from 'expo-location';
+import MapView, { Marker } from 'react-native-maps';
+
 
 export default function Landing() {
   const handleGetRequest = async () => {
     try {
-      const response = await axios.get('http://192.168.1.51:3002/hola');
+      const response = await axios.post('http://192.168.1.51:3002/hola',{
+        "addresLocal": "5HTJkawMqHSvVRi2XrE7vdTU4t5Vq1EDv2ZDeWSwNxmmQKEK",
+        "accountTo": "5G8mzxiCCW4VALGRGdaqGPfrMLp7CeaVfk5XwPhDDaDyGEgE",
+        "quantity": 10
+      });
       Alert.alert('Respuesta:', JSON.stringify(response.data));
     } catch (error) {
       Alert.alert('Error:', error.message);
@@ -16,6 +23,29 @@ export default function Landing() {
   const video = React.useRef(null);
   const [status, setStatus] = React.useState({});
   const navigation = useNavigation();
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = `Latitude: ${location.coords.latitude}, Longitude: ${location.coords.longitude}`;
+  }
+
 
   return (
     <View style={styles.container}>
@@ -37,7 +67,28 @@ export default function Landing() {
         Make GET Request 
       </Text>
     </TouchableOpacity>
-
+    {location ? (
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        >
+          <Marker
+            coordinate={{
+              latitude: location.coords.latitude,
+              longitude: location.coords.longitude,
+            }}
+            title="Mi ubicación"
+            description="Aquí estoy"
+          />
+        </MapView>
+      ) : (
+        <Text>Cargando...</Text>
+      )}
   </View>
   );
 }
@@ -56,6 +107,32 @@ const styles = StyleSheet.create({
   },
   contButton: {
     // position: "absolute",
+    alignSelf: 'center',
+    height: 50,
+    width: 200,
+    justifyContent: 'center',
+    alignItems: 'center',
+    bottom: 0,
+    backgroundColor: '#3AA940',
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: '#000',
+    top: 40
+  },
+  map: {
+    alignSelf: 'center',
+    height: 300,
+    width: 300,
+    justifyContent: 'center',
+    alignItems: 'center',
+    bottom: 0,
+    backgroundColor: '#3AA940',
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: '#000',
+    top: 40
+  },
+  containertwo:{
     alignSelf: 'center',
     height: 50,
     width: 200,
